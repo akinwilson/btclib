91886fc293b5af66a7bf3a1bb3ef949b4d1454d9
nameduser aconite <nameduser@aconite.com>
2025-01-25 11:10:06 +0000

    fixed formatting issue. There were leading zeros SOMETIMES prepred which were dropped when converting to integer. But for the sake of serialization, these leading zeros are needed

--- src/btclib/ecds.py
+++ src/btclib/ecds.py
@@ -83,6 +83,7 @@ class Signature:
 			if rxbin[0] & 0x80:
 				rxbin = b'\x00' + rxbin
 			result = bytes([2, len(rxbin)]) + rxbin
+			
 			sigbin = self.value.to_bytes(32, 'big')
 			sigbin = sigbin.lstrip(b"\x00")
 			if sigbin[0] & 0x80:
@@ -91,52 +92,86 @@ class Signature:
 			return bytes([0x30, len(result)]) + result 
 
 		else:
-			rxbin = self.r_x.to_bytes(32, 'big')
+			rxbin = self.r_x.to_bytes(32, 'big') # 
 			# removing all null bytes
-			rxbin = rxbin.lstrip(b"\x00")
+			rxbin = rxbin.lstrip(b"\x00") # 
 			# if rxbin has high bit add \x00
+			prepend  = False
 			if rxbin[0] & 0x80:
+				prepend = True 
 				rxbin = b'\x00' + rxbin
-			
-			r_x_value = lp + f"{hex(int.from_bytes(rxbin, 'big'))}".lstrip("0x") + r 
-			r_x_length =  lc  + f"{hex(len(rxbin))}".lstrip('0x') + r 
+
+			if prepend:
+				r_x_value = lp + '00' + f"{int.from_bytes(rxbin, 'big'):x}" + r
+			else: 
+				r_x_value = lp + f"{int.from_bytes(rxbin, 'big'):x}" + r
+
+			print("r_x value (base 16)",  r_x_value)
+			r_x_length =  lc  + f"{len(rxbin):x}" + r
+			print("length of r_x (base 16): ", r_x_length)
 			r_prefxbyte = re + "02" + r
-		 	 # b'\x02'
+			print("r_prefix byte (base 16) byte container", r_prefxbyte)
+		 	
+			r_x_part = r_prefxbyte + r_x_length + r_x_value
+			print(f"r_x EXPECT:{r_x_part}\n\n")
 			result = bytes([2, len(rxbin)]) + rxbin
 
 			sigbin = self.value.to_bytes(32, 'big')
 			# print("sigbin[0] before strip", sigbin[0])
 			sigbin = sigbin.lstrip(b"\x00")
 			# print("sigbin[0]", sigbin[0])
+			prepend = False 
 			if sigbin[0] & 0x80:
-				sigbin = b'\x00' + sigbin
+				prepend = True 
+				sigbin = b'\x00' + sigbin	
+			if prepend:
+				sig_value =  y + '00' + f"{int.from_bytes(sigbin, 'big'):x}" + r
+			else: 
+				sig_value =  y +  f"{int.from_bytes(sigbin, 'big'):x}" + r
+				
+			sig_length = gr  +  f"{len(sigbin):x}" + r
+			print("sig length (base 16):", sig_length)
+			
+
+			## manually prepending the 
+	
+			print("sig value (base 16): ", sig_value)
+			sig_prefxbyte =  bb + '02' + r
+			print("sig prefix byte (base 16) byte container:", sig_prefxbyte)
+			sig_part = sig_prefxbyte + sig_length + sig_value
+			print(f"sig EXPECT: ", sig_part )
+
 			result += bytes([2, len(sigbin)]) + sigbin
 			
+			# print("int.from_bytes(sigbin, 'big'):x}.lstrip('0x')", f"{int.from_bytes(sigbin, 'big'):x}".lstrip("0x").zfill(64))
+			# print("siglen",f"{int.from_bytes(sigbin, 'big'):x}".lstrip("0x").__len__())
 
 			sig_value =  y + f"{int.from_bytes(sigbin, 'big'):x}".lstrip("0x") + r
 			sig_length = gr  + f"{len(sigbin)}".lstrip('0x') + r
 			sig_prefxbyte =  bb + '02' + r
 			
-
-			prefix = g + f"{hex(0x30)}".lstrip("0x") + r + w + f"{hex(len(result))}".lstrip("0x") + r
+			marker_byte =  g + '30' + r
+			
+			print("\n\nPrefix byte (base 16): ", marker_byte)
+			total_serial_len = w + f"{len(result):x}" + r
+			print("total sig length (base16): ", total_serial_len)
 
 			print()
 			print("Distinguished encoding representation for signatures in hex base. der signature serialization:")
 			print()
-			print(prefix + r_prefxbyte +r_x_length + r_x_value  + sig_prefxbyte + sig_length +  sig_value )
+			print(marker_byte + total_serial_len + r_x_part + sig_part)
 			print()
 			print("key:")
 			print(g + "Signature prefix "+ r )
 			print(w+ "Length of complete signature" + r)
-			print(y + "signature value " + r )
-			print(gr + "signature length " + r )
-			print(bb + "signature prefix " + r )
-			
+			print(re + "r_x prefix " + r )
 			print(lc + "r_x length " + r )
 			print(lp + "r_x value " + r )
-			print(re + "r_x prefix " + r )
+			print(bb + "signature prefix " + r )
+			print(gr + "signature length " + r )
+			print(y + "signature value " + r )
 			print()
-
+			return bytes([0x30, len(result)]) + result 
 
 			
 

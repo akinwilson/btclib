19691a49120515840d2e6fb17c6fb2c79fe72af2
nameduser aconite <nameduser@aconite.com>
2025-01-26 18:10:08 +0000

    had to include a dummy script hex to be able to parse the sample transaction for testing, will remove once Script has been developed

--- src/btclib/transaction.py
+++ src/btclib/transaction.py
@@ -3,8 +3,17 @@ import requests
 from io import BytesIO
 import json 
 
-# from .script import Script 
+from .script import Script 
 from typing import BinaryIO
+
+# script_hex = ('6b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c50'
+#               '31ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3'
+#               'f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e'
+#         '3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278a')
+SCRIPT_HEX = ('6b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c50'
+            '31ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3'
+            'f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e'
+            '3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278a')
 
 class TxFetcher:
     cache = {}
@@ -104,14 +113,57 @@ class Tx:
     def parse(cls, s:BinaryIO, testnet : bool = False):
         v_bytes = s.read(4)
         version = int.from_bytes(v_bytes, 'little')
-
+        print("verison: ", version)
         num_inputs = Varint.decode(s)
+        print("num inputs", num_inputs)
         inputs =[TxIn.parse(s) for _ in range(num_inputs)]
+        # when pass the stream into the above class, its 
+        # is not reading move the pointer along.
+
+
+        # this will be produced by the soon-to-be-defined Script class
+        # until then, hard-coding its value
+        #######################################################################
+        matched = False
+        test_hex = ''
+        while not matched:
+            test_hex += s.read(1).hex()
+            if test_hex == SCRIPT_HEX:
+                break 
+        ##########################################################################
+
+        # sequences = [int.from_bytes(s.read(4), 'little') for _ in range(num_inputs)]
+        # print("After extracting TX in info, ")
+        # print(f"Look for sequence: feffffff")
+        # print("The next 27*4 bytes in hex are:",s.read(27*4).hex() )
+        sequence = int.from_bytes(s.read(4), 'little') 
+                 
+        for i in range(len(inputs)):
+            inputs[i].sequence = sequence
+
+
+        # print(f"What are the next 4 bytes?",sequences )
+        # print(inputs)
+
+
+        num_outputs = Varint.decode(s)
+        outputs = [TxOut.parse(s) for _ in range(num_outputs)]
+        locktime = int.from_bytes(s.read(4), 'little')
+        # print(f"locktime {locktime}")
+        # for (tx_in, seq) in zip(inputs, sequences):
+        #     tx_in.sequence = seq
+
+        # inputs = [tx_in.sequence = seq for (tx_in, seq) in zip(inputs, sequences)]
+        # TxOut.parse(s)
+        # num_outputs = Varint.decode(s)
+        # outputs = TxOut.parse(s)
         # tx_in  = TxIn.parse(s)
         # print(f'{version=}')
         # print(f'{num_inputs=}')
+        # outputs = TxOut.parse(s)
 
-        return Tx(version=version, tx_outs=None, locktime=None, tx_ins=inputs, testnet=testnet)
+
+        return Tx(version=version, tx_outs=outputs, locktime=locktime, tx_ins=inputs, testnet=testnet)
     
 
 
@@ -122,10 +174,10 @@ class TxIn:
         self.prev_tx = prev_tx
         self.prev_index = prev_index
         
-        # if script_sig is None:
-        #     self.script_sig = Script()
-        # else:
-        #     self.script_sig = script_sig
+        if script_sig is None:
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
         
         
         self.sequence = sequence
@@ -161,9 +213,14 @@ class TxIn:
     @classmethod
     def parse(cls, s:BinaryIO):
         prev_tx_hash_bytes = s.read(32)[::-1]
+        print('prev_tx_hash_bytes', prev_tx_hash_bytes.hex())
         # prev_tx_hash = int.from_bytes(prev_tx_hash_bytes, 'little')
-        prev_tx_idx = int.from_bytes(s.read(4), 'little') 
-        # script_sig = Script.parse(s)
+        prev_tx_idx = int.from_bytes(s.read(4), 'little')
+
+        print("previous tx id :", prev_tx_idx)
+        
+        # print("Byte stream left: ", len(s.read()))
+        # script_sig = Script.parse(s)cr
         # prev_tx_idx = int.from_bytes(prev_tx_idx_bytes, 'little')
         return TxIn(prev_tx=prev_tx_hash_bytes, prev_index=prev_tx_idx)
 
@@ -183,6 +240,29 @@ class TxOut:
         result = self.amount.to_bytes(8, "little")
         result += self.script_pubkey.serialize()
         return result
+    
+    @classmethod
+    def parse(cls, s:BinaryIO):
+        # num_outputs = Varint.decode(s)
+        # print(f"{num_outputs=}") #this is correct 
+        # TxOut()
+        # trying to extract he amount is near imposssible 
+        # print("s.read(8) to little int", int.from_bytes(s.read(8), 'little'))
+        amount = int.from_bytes(s.read(8), 'little')        
+        # print(f"{amount=}")
+        script_pub_key_len = Varint.decode(s)
+        # print(s.read(8).hex())
+        script_pub_key = s.read(script_pub_key_len).hex() # , 'little')
+        # print(f"{script_pub_key_len=}")
+        # print(f"{script_pub_key=}")
+        # amount = int.from_bytes(s.read(8), 'little')
+        return TxOut(amount=amount, script_pubkey=script_pub_key)
+        # num_outputs = Varint.decode(s) # .read(4), 'little')
+        # print(f"{num_outputs=}")
+        # outputs = [TxOut.parse(s) for _ in range(num_outputs)]
+        # print(f"{num_outputs=}")
+        # print(f"{outputs=}")
+        # return outputs
 
 
 

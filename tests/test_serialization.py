d7d5dbf84aab528bed51995f15721af18687c638
nameduser aconite <nameduser@aconite.com>
2025-01-27 12:40:06 +0000

    added test for serialiation of TX

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,7 +1,32 @@
 from btclib.ec import S256Point, SecretKey
 from btclib.ecds import Signature
+from btclib.transaction import Tx
 from btclib.constants import Gx, Gy, SECP256K1_A,SECP256K1_B
 import pytest 
+from io import  BytesIO
+
+# testing serialisation of transactions 
+
+
+tx_hex = (
+ '0100000001813f79011acb80925dfe69b3def355fe914bd1d9'
+ '6a3f5f71bf8303c6a989c7d1000000006b483045022100ed'
+ '81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf213'
+ '20b0277457c98f02207a986d955c6e0cb35d446a89d3f5610'
+ '0f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3'
+ '624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b27'
+ '8afeffffff02a135ef01000000001976a914bc3b654dca7e56b'
+ '04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141'
+ 'c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
+
+
+# need to parameterised this for more than just one tx 
+def test_parsing_and_serialization_of_tx(tx_hex):
+    tx = Tx.parse(BytesIO(bytes.fromhex(tx_hex)))
+    tx.serialize().hex() == tx_hex
+
+
+
 
 
 test_cases = [(5003, True, True, 'cMahea7zqjxrtgAbB7LSGbcQUr1uX1ojuat9jZodMN8rFTv2sfUK'),
